a36a12733b73bfe2d5a2d6e5b22f52db020d68cf
Julio3215 <alexander123julio@gmail.com>
2026-08-25 23:50:54 -0500

    Refactorizacion completa de API Gateway con arquitectura modular

--- gateway/src/utils/index.js
+++ gateway/src/utils/index.js
@@ -1,16 +1,7 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { APP_SECRET } = require('../config');
+const appErrors = require('./app-errors');
+const errorHandler = require('./error-handler');
 
 module.exports = {
-    GenerateSalt: async () => bcrypt.genSalt(),
-
-    GeneratePassword: async (password, salt) => bcrypt.hash(password, salt),
-
-    ValidatePassword: async (enteredPassword, savedPassword, salt) =>
-        (await bcrypt.hash(enteredPassword, salt)) === savedPassword,
-
-    GenerateSignature: async (payload) => jwt.sign(payload, APP_SECRET, { expiresIn: '1d' }),
-
-    FormateData: (data) => ({ data }),
+    ...appErrors,
+    ErrorHandler: errorHandler,
 };
